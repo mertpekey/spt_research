60d1e4bdda56d775648db92758908c53f924b651
Mert Pekey <mpekey@gmail.com>
2025-01-14 15:07:35 +0300

    bugfix

--- src/dataset.py
+++ src/dataset.py
@@ -23,7 +23,6 @@ class SpatialDataset(Dataset):
         protein_data = self.protein_data[idx]
         
         if self.processor:
-            spot_patch = self.processor(images=spot_patch, return_tensors="pt")["pixel_values"].squeeze(0)
             # Check processor type by its interface/behavior
             if hasattr(self.processor, '__call__') and hasattr(self.processor, 'transforms'):
                 # Torchvision/TIMM style transforms (like UNI model)
